26e64568d0b06f1be92c7d232549d48797aeb70a
Yohei Minami <yohei.minami@accenture.com>
2022-09-06 13:17:34 +0900

    test

--- index.js
+++ index.js
@@ -1,28 +1,26 @@
 const $x = document.querySelector('.x');
 const $y = document.querySelector('.y');
 const $z = document.querySelector('.z');
+const $button = document.querySelector('button');
 
-window.addEventListener('devicemotion', function(event) {
-  console.log(event.acceleration.x + ' m/s2');
-  $x.innerText = event.acceleration.x;
-  $y.innerText = event.acceleration.y;
-  $z.innerText = event.acceleration.z;
-});
+$button.addEventListener('click', function() {
 
-if (DeviceMotionEvent.requestPermission) {
-  DeviceMotionEvent.requestPermission().then(permissionState => {
-    if (permissionState === 'granted') {
-      window.addEventListener("devicemotion", (event) => {
-        if (!event.accelerationIncludingGravity) {
-          alert('event.accelerationIncludingGravity is null');
-          return;
-        }
-        $x.innerText = event.acceleration.x;
-        $y.innerText = event.acceleration.y;
-        $z.innerText = event.acceleration.z;
-      })
-    }
-  }).catch(console.error);
-} else {
-  alert('DeviceMotionEvent.requestPermission is not found')
-}
+  if (DeviceMotionEvent.requestPermission) {
+    DeviceMotionEvent.requestPermission().then(permissionState => {
+      if (permissionState === 'granted') {
+        window.addEventListener("devicemotion", (event) => {
+          if (!event.accelerationIncludingGravity) {
+            alert('event.accelerationIncludingGravity is null');
+            return;
+          }
+          $x.innerText = event.acceleration.x;
+          $y.innerText = event.acceleration.y;
+          $z.innerText = event.acceleration.z;
+        })
+      }
+    }).catch(console.error);
+  } else {
+    alert('DeviceMotionEvent.requestPermission is not found')
+  }
+
+})
