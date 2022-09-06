4c615e20aaa83a9b50f79440d8f4f1dba08b90b2
Yohei Minami <yohei.minami@accenture.com>
2022-09-06 13:22:22 +0900

    test

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const $x = document.querySelector('.x');
 const $y = document.querySelector('.y');
 const $z = document.querySelector('.z');
+const $mx = document.querySelector('.mx');
+const $my = document.querySelector('.my');
+const $mz = document.querySelector('.mz');
 const $button = document.querySelector('button');
+let maxX = 0;
+let maxY = 0;
+let maxZ = 0;
 
 $button.addEventListener('click', function() {
 
@@ -16,6 +22,12 @@ $button.addEventListener('click', function() {
           $x.innerText = event.acceleration.x;
           $y.innerText = event.acceleration.y;
           $z.innerText = event.acceleration.z;
+          maxX = Math.max(maxX, Math.abs(event.acceleration.x));
+          maxY = Math.max(maxY, Math.abs(event.acceleration.y));
+          maxZ = Math.max(maxZ, Math.abs(event.acceleration.z));
+          $mx.innerText = maxX;
+          $my.innerText = maxY;
+          $mz.innerText = maxZ;
         })
       }
     }).catch(console.error);
